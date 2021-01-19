68ca66b9aba27afcfe146b6ad656d00262a46fc8
MrMyatNoe <thetmyatnoe223@gmail.com>
2021-01-19 22:50:01 +0630

    fix:change capitalize to capitalizedAfter

--- basics.py
+++ basics.py
@@ -1,14 +1,14 @@
 def sentence_maker(phrase):
     interrogatives = ('why','how','what','where','who','when')
     greetings = ('hi','hello','oh','yeah','yes','got it','hey','dude')
-    captialized = phrase.capitalize()
+    captializedAfter = phrase.capitalize()
     if phrase.startswith(interrogatives):
-        return "{}?".format(captialized)
+        return "{}?".format(captializedAfter)
     elif phrase.startswith(greetings):
-        return "{}!".format(captialized)
+        return "{}!".format(captializedAfter)
     else:
-        return "{}.".format(captialized)
-
+        return "{}.".format(captializedAfter)
+        
 result = []
 while True:
     user_input = input("Say Something : ")
